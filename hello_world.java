79629f500b043f6d7af40a4986549031db80240c
alutton <andrew.lutton@uvm.edu>
2013-11-10 10:26:00 -0500

    Changed some strings

--- hello_world.java
+++ hello_world.java
@@ -3,8 +3,9 @@ public class Hello_World {
 
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		
+		System.out.println("Hello WORLD!");
+		System.out.println("Java is great!");
+		System.out.println("Git IS very useful");
         
 	}
 
